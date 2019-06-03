dea66c7045e6bdab2fd999708bd49b554d3ebeaf
Niki Jiandani <niki.jiandani@gmail.com>
2019-06-03 23:00:04 +0000

    Add HTTP headers to getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,16 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  console.log('This works good!');
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
