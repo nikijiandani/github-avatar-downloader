dfabfaa2066962c52fd1b2b1b6e118a42e298a22
Niki Jiandani <niki.jiandani@gmail.com>
2019-06-03 22:39:11 +0000

    Add and call getRepoContributors functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  console.log('This works good!');
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
