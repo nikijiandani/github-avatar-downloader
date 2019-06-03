fa2089408ffb80f55f35bd643ea32a93bf37c8f7
Niki Jiandani <niki.jiandani@gmail.com>
2019-06-03 23:30:50 +0000

    Add secrets to gitignore & add HTTP header with Auth

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,14 @@
+var secret = require('./secrets');
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors?client_secret=" + secret ,
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': secret.GITHUB_ID,
+      'Authorization': "token " + secret.GITHUB_TOKEN
     }
   };
 
