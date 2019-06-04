48c5684daa31afabcf46ddda3b6af5fc82d89342
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-03 20:48:08 -0400

    Implement downloadImageByUrl functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var secret = require('./secrets');
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -18,9 +19,30 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+    console.log("Errors:", err); 
+    })
+    .on('response', function (response) {
+      console.log('Downloading image...'); 
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function () {
+      console.log("Download complete.")
+    });   
+}
+
+// downloadImageByURL('https://avatars1.githubusercontent.com/u/43004?v=4', 'avatars/pbakaus.jpg');
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
+  const dirName = './avatars';
+  if(!fs.existsSync(dirName)){
+    fs.mkdirSync(dirName);
+  }
   result.forEach(element => {
-    console.log(element["avatar_url"]);
+    downloadImageByURL(element['avatar_url'], dirName + '/' + element['login'] + '.jpg');
   });;
 });
